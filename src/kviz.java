25d003017419c481b92ff42c11a95afe956d7ab5
Ljubisa Savic <ljsavic90@gmail.com>
2018-02-07 11:47:58 +0100

    Initial commit, uradjene metode

---
+++ src/kviz.java
@@ -0,0 +1,24 @@
+
+public class kviz {
+	
+	
+    public static int randomSabiranje(int a, int b){
+		// Dino
+	}
+	public static int randomOduzimanje(int a, int b){
+		// Dino
+	}
+	public static int randomMnozenje(int a, int b){
+		
+	}
+	public static int randomDjeljenje(int a, int b){
+		
+	}
+	public static int randomKorjenovanje(int a){
+		// Eldin
+	}
+	public static int randomKvadriranje(int b){
+		// Eldin
+	}
+	
+}
